2c69e449051aab542080aa33dcce9694fb8f1e47
Jénaïc Cambré <jenaic@Jenaics-MacBook-Air.local>
2019-05-22 15:07:59 +0200

    fix not capitalized middle names

--- src/pdf/renderPage.js
+++ src/pdf/renderPage.js
@@ -1,14 +1,22 @@
-const fontPath = './fonts/';
+const fontPath = "./fonts/";
 // const FONT_NAME = `${fontPath}PlayfairDisplay-BoldItalic.ttf`;
 const FONT_NAME = `${fontPath}MonotypeCorsiva.ttf`;
 const FONT_DESCRIPTION = `${fontPath}LibreBaskerville-Regular.ttf`;
 const FONT_BODY = `${fontPath}Roboto-Regular.ttf`;
 
-const COLOR_NAME = '#222222';
-const COLOR_DESCRIPTION = '#8c8c8c';
+const COLOR_NAME = "#222222";
+const COLOR_DESCRIPTION = "#8c8c8c";
 
 const tplPath = `./fixtures/diploma_ai.png`;
 const logoKonexio = `./fixtures/logo-konexio-final2.png`;
+
+const capitalize = str => {
+  const words = str.split(" ");
+  const capitalized = words.map(word => {
+    return word.substring(0, 1).toUpperCase() + word.substring(1).toLowerCase();
+  });
+  return capitalized.join(" ");
+};
 
 const renderPage = ({
   doc,
@@ -21,88 +29,87 @@ const renderPage = ({
   awarderRightName,
   awarderRightRole
 }) => {
-  const firstName = studentFirstName.substring(0, 1).toUpperCase() + studentFirstName.substring(1).toLowerCase();
+  const firstName = capitalize(studentFirstName);
   const surname = studentSurname.toUpperCase();
 
   doc.image(tplPath, 0, 0);
 
-doc.image(logoKonexio, 240, 635, {
-  width: 240
-});
-
-doc.image(logoKonexio, 1025, 635, {
-  width: 240
-});
-
-doc
-  .font(FONT_NAME)
-  .fillColor(COLOR_NAME)
-  .fontSize(80)
-  .text(`${firstName} ${surname}`, 150, 400, {
-    width: 1200,
-    align: 'center'
+  doc.image(logoKonexio, 240, 635, {
+    width: 240
   });
 
-
-// delivery date
-doc
-  .font(FONT_DESCRIPTION)
-  .fontSize(40)
-  .fillColor(COLOR_DESCRIPTION)
-  .text(deliveryDate, 550, 525, {
-    width: 450,
-    align: 'center'
+  doc.image(logoKonexio, 1025, 635, {
+    width: 240
   });
 
-// description diploma
-doc
-  .font(FONT_BODY)
-  .fontSize(25)
-  .fillColor(COLOR_DESCRIPTION)
-  .text(descriptionWorkshop, 550, 590, {
-    width: 450,
-    align: 'center'
-  });
+  doc
+    .font(FONT_NAME)
+    .fillColor(COLOR_NAME)
+    .fontSize(80)
+    .text(`${firstName} ${surname}`, 150, 400, {
+      width: 1200,
+      align: "center"
+    });
 
-// awarder left name
-doc
-  .font(FONT_BODY)
-  .fontSize(25)
-  .fillColor(COLOR_DESCRIPTION)
-  .text(awarderLeftName, 220, 870, {
-    width: 310,
-    align: 'center'
-  });
+  // delivery date
+  doc
+    .font(FONT_DESCRIPTION)
+    .fontSize(40)
+    .fillColor(COLOR_DESCRIPTION)
+    .text(deliveryDate, 550, 525, {
+      width: 450,
+      align: "center"
+    });
 
-// awarder left role
-doc
-  .font(FONT_BODY)
-  .fontSize(25)
-  .fillColor(COLOR_DESCRIPTION)
-  .text(awarderLeftRole, 220, 900, {
-    width: 310,
-    align: 'center'
-  });
+  // description diploma
+  doc
+    .font(FONT_BODY)
+    .fontSize(25)
+    .fillColor(COLOR_DESCRIPTION)
+    .text(descriptionWorkshop, 550, 590, {
+      width: 450,
+      align: "center"
+    });
 
-// awarder right name
-doc
-  .font(FONT_BODY)
-  .fontSize(25)
-  .fillColor(COLOR_DESCRIPTION)
-  .text(awarderRightName, 985, 870, {
-    width: 310,
-    align: 'center'
-  });
+  // awarder left name
+  doc
+    .font(FONT_BODY)
+    .fontSize(25)
+    .fillColor(COLOR_DESCRIPTION)
+    .text(awarderLeftName, 220, 870, {
+      width: 310,
+      align: "center"
+    });
 
-// awarder right role
-doc
-  .font(FONT_BODY)
-  .fontSize(25)
-  .fillColor(COLOR_DESCRIPTION)
-  .text(awarderRightRole, 985, 900, {
-    width: 310,
-    align: 'center'
-  });
+  // awarder left role
+  doc
+    .font(FONT_BODY)
+    .fontSize(25)
+    .fillColor(COLOR_DESCRIPTION)
+    .text(awarderLeftRole, 220, 900, {
+      width: 310,
+      align: "center"
+    });
+
+  // awarder right name
+  doc
+    .font(FONT_BODY)
+    .fontSize(25)
+    .fillColor(COLOR_DESCRIPTION)
+    .text(awarderRightName, 985, 870, {
+      width: 310,
+      align: "center"
+    });
+
+  // awarder right role
+  doc
+    .font(FONT_BODY)
+    .fontSize(25)
+    .fillColor(COLOR_DESCRIPTION)
+    .text(awarderRightRole, 985, 900, {
+      width: 310,
+      align: "center"
+    });
 };
 
 module.exports = renderPage;
